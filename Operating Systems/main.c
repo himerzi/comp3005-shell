e71ff0eb41c74fd186d53c1802a9f72b6af029e0
Michael Detmold <mdetmold@gmail.com>
2014-10-27 13:10:34 +0000

    Initial Commit

---
+++ Operating Systems/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Operating Systems
+//
+//  Created by Michael Detmold on 27/10/2014.
+//  Copyright (c) 2014 Michael Detmold. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
